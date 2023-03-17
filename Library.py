fd1ec91909c4f2cd042df6753212742a4faaf9fd
AZAZ3LL0 <sadrievsamat4@gmail.com>
2023-03-17 17:39:54 +0300

    Second commit: update classes

--- Library.py
+++ Library.py
@@ -1,7 +1,14 @@
 import psycopg2
+import enum
+import datetime
 
 
-class Library:
+class RequestCodes(enum.Enum):
+    POST = 0
+    GET = 1
+
+
+class DataBaseHandler:
 
     def __init__(self, database, host, user, password, port):
         self.conn = psycopg2.connect(database=database,
@@ -9,26 +16,81 @@ class Library:
                                      password=password,
                                      host=host,
                                      port=port)
-        self.cursor = self.conn.cursor()
 
-#CRUD books
+    def handle_request(self, request, values, request_code):
+        cursor = self.conn.cursor()
+        cursor.execute(request, values)
+        if request_code == RequestCodes.POST:
+            self.conn.commit()
+        if request_code == RequestCodes.GET:
+            return cursor.fetchall()
 
-    def create_book(self, title, author, year, is_available):
-        sql = "INSERT INTO books (title, author, year,is_available) VALUES (%s, %s, %s, %s)"
-        values = (title, author, year, is_available)
-        self.cursor.execute(sql, values)
-        self.conn.commit()
+
+class AbstractManager:
+    def __init__(self, database, host, user, password, port):
+        self.dbh = DataBaseHandler(database, host, user, password, port)
+
+
+class UserManager(AbstractManager):
+
+    # CRUD users
+
+    def create_user(self, name, email, phone):
+        sql = "INSERT INTO users (name, email, phone) VALUES (%s, %s, %s)"
+        values = (name, email, phone)
+        self.dbh.handle_request(sql, values, RequestCodes.POST)
+
+    def read_users(self):
+        sql = "SELECT * FROM users "
+        values = ()
+        return self.dbh.handle_request(sql, values, RequestCodes.GET)
+
+    def read_user(self, user_id):
+        sql = "SELECT * FROM users WHERE id = %s"
+        return self.dbh.handle_request(sql, (user_id,), RequestCodes.GET)
+
+    def update_user(self, user_id, name=None, email=None, phone=None):
+        sql = "UPDATE users SET "
+        values = []
+        if name is not None:
+            sql += "name = %s, "
+            values.append(name)
+        if email is not None:
+            sql += "email = %s, "
+            values.append(email)
+        if phone is not None:
+            sql += "phone = %s, "
+            values.append(phone)
+        if not values:
+            return
+        sql = sql[:-2] + " WHERE id = %s"
+        values.append(user_id)
+        self.dbh.handle_request(sql, values, RequestCodes.POST)
+
+    def delete_user(self, user_id):
+        sql = "DELETE FROM users WHERE id = %s"
+        self.dbh.handle_request(sql, (user_id,), RequestCodes.POST)
+
+
+class BookManager(AbstractManager):
+
+    # CRUD books
+
+    def create_book(self, title, author, year, count):
+        sql = "INSERT INTO books (title, author, year,count) VALUES (%s, %s, %s, %s)"
+        values = (title, author, year, count)
+        self.dbh.handle_request(sql, values, RequestCodes.POST)
 
     def read_books(self):
-        self.cursor.execute("SELECT * FROM books")
-        return self.cursor.fetchall()
+        sql = "SELECT * FROM books "
+        values = ()
+        return self.dbh.handle_request(sql, values, RequestCodes.GET)
 
     def read_book(self, book_id):
         sql = "SELECT * FROM books WHERE id = %s"
-        self.cursor.execute(sql, (book_id,))
-        return self.cursor.fetchone()
+        return self.dbh.handle_request(sql, (book_id,), RequestCodes.GET)
 
-    def update_book(self, book_id, title=None, author=None, year=None):
+    def update_book(self, book_id, title=None, author=None, year=None, count=None):
         sql = "UPDATE books SET "
         values = []
         if title is not None:
@@ -40,79 +102,31 @@ class Library:
         if year is not None:
             sql += "year = %s, "
             values.append(year)
+        if count is not None:
+            sql += "count = %s, "
+            values.append(count)
         if not values:
             return
         sql = sql[:-2] + " WHERE id = %s"
         values.append(book_id)
-        self.cursor.execute(sql, tuple(values))
-        self.conn.commit()
+        self.dbh.handle_request(sql, values, RequestCodes.POST)
 
     def delete_book(self, book_id):
         sql = "DELETE FROM books WHERE id = %s"
-        self.cursor.execute(sql, (book_id,))
-        self.conn.commit()
+        self.dbh.handle_request(sql, (book_id,), RequestCodes.POST)
 
-#CRUD users
-
-    def create_user(self, name, email):
-        sql = "INSERT INTO users (name, email) VALUES (%s, %s)"
-        values = (name, email)
-        self.cursor.execute(sql, values)
-        self.conn.commit()
-
-    def read_users(self):
-        self.cursor.execute("SELECT * FROM users")
-        return self.cursor.fetchall()
-
-    def read_user(self, user_id):
-        sql = "SELECT * FROM users WHERE id = %s"
-        self.cursor.execute(sql, (user_id,))
-        return self.cursor.fetchone()
-
-    def update_user(self, user_id, name=None, email=None):
-        sql = "UPDATE users SET "
-        values = []
-        if name is not None:
-            sql += "name = %s, "
-            values.append(name)
-        if email is not None:
-            sql += "email = %s, "
-            values.append(email)
-        if not values:
-            return
-        sql = sql[:-2] + " WHERE id = %s"
-        values.append(user_id)
-        self.cursor.execute(sql, tuple(values))
-        self.conn.commit()
-
-    def delete_user(self, user_id):
-        sql = "DELETE FROM users WHERE id = %s"
-        self.cursor.execute(sql, (user_id,))
-        self.conn.commit()
+    def take_book(self, user_id, book_id, time_taken):
+        sql = "INSERT INTO user_book (user_id, book_id, time_taken) VALUES (%s, %s, %s)"
+        values = (user_id, book_id, time_taken)
+        self.dbh.handle_request(sql, values, RequestCodes.POST)
 
 
-
-
-library = Library("library", "localhost", "postgres", "letu4i_1wsv9_tsv9", "5432")
-
-# Create a book
-library.create_book("буратино", "амир", 2003, True)
-
-# Read all books
-books = library.read_books()
-for book in books:
-    print(book)
-
-# Read a specific book
-book = library.read_book(1)
-print(book)
-
-# Update a book
-library.update_book(1, "The Great Gatsby", "F. Scott Fitzgerald")
-
-# Delete a book
-library.delete_book(1)
-
-
-# Update user
-library.update_user(1, "samat", "sami4@gmail.com")
+# um = UserManager("library", "localhost", "postgres", "letu4i_1wsv9_tsv9", "5432")
+# user = um.read_user(1)
+# print(*user)
+# user = um.update_user(1,'anoxin','HUILA.com','228_228_228')
+# um.delete_user(2)
+# um.create_user('Samat', 'ssi.com', 123456789)
+# bk = BookManager("library", "localhost", "postgres", "letu4i_1wsv9_tsv9", "5432")
+# bk.create_book('Маша и Медведь', 'Пшкин', 1920, 3)
+# bk.take_book(3, 5, datetime.datetime.now())
